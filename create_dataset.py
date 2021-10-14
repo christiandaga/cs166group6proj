a0ab6893e67fd2ad4989832440ef8856d5da4c04
sudarshankannan <sudarshan.kannan99@gmail.com>
2021-10-13 21:54:01 -0700

    hello world

--- create_dataset.py
+++ create_dataset.py
@@ -3,17 +3,18 @@ import pandas as pd
 import tweepy
 import config
 
-FILENAME = "twitter_human_bots_dataset.csv"
-
-#load kaggle dataset into a dataframe
-pd.read_csv(FILENAME)
-
-#configure twitter api client
+#configure token stuff
 access_token = config.access_token
 access_token_secret = config.access_token_secret
 consumer_key = config.consumer_key
 consumer_secret = config.consumer_secret
 
+#load kaggle dataset into a dataframe
+FILENAME = "twitter_human_bots_dataset.csv"
+twitter_dataframe = pd.read_csv(FILENAME)
+print(twitter_dataframe.head())
+
+#configure twitter api client
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
