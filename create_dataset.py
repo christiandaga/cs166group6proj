258bd7f1ce9f57734341f7b6b1f1f21ff6ed25b6
sudarshankannan <sudarshan.kannan99@gmail.com>
2021-10-13 21:31:06 -0700

    hello

--- create_dataset.py
+++ create_dataset.py
@@ -3,3 +3,15 @@ import pandas as pd
 import tweepy
 
 FILENAME = "twitter_human_bots_dataset.csv"
+
+#load kaggle dataset
+
+#configure twitter api client
+
+#obtain fields from accounts
+
+#insert profile data points into corresponding rows
+
+#remove any junk rows
+
+#export csv
