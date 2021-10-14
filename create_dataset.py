71af3a17e9c1cedb25535388ae60cb553ad86b48
sudarshankannan <sudarshan.kannan99@gmail.com>
2021-10-13 21:43:57 -0700

    added to gitignore and updated config.py

--- create_dataset.py
+++ create_dataset.py
@@ -1,14 +1,24 @@
 import numpy as np
 import pandas as pd
 import tweepy
+import config
 
 FILENAME = "twitter_human_bots_dataset.csv"
 
-#load kaggle dataset
+#load kaggle dataset into a dataframe
+pd.read_csv(FILENAME)
 
 #configure twitter api client
+access_token = config.access_token
+access_token_secret = config.access_token_secret
+consumer_key = config.consumer_key
+consumer_secret = config.consumer_secret
 
-#obtain fields from accounts
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+#obtain fields from accounts using api client
 
 #insert profile data points into corresponding rows
 
