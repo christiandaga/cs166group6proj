807f513f300a45afab104a9e540077602be7b30a
au01gh <59246763+au01gh@users.noreply.github.com>
2021-11-27 16:05:54 -0800

    Cleaned up da code pt II

--- predict.py
+++ predict.py
@@ -4,7 +4,7 @@ import tweepy
 import config
 import sys
 
-#configure token stuff
+#Tokens
 access_token = config.access_token
 access_token_secret = config.access_token_secret
 consumer_key = config.consumer_key
@@ -14,7 +14,6 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, parser=tweepy.parsers.JSONParser(), wait_on_rate_limit=True)
 
-
 try:
     user_id = sys.argv[1]
     twitter_user = api.get_user(params={"user_id":user_id})
@@ -23,14 +22,17 @@ try:
         "friends_count": twitter_user["friends_count"],
         "statuses_count": twitter_user["statuses_count"],
     }
+
     print(current_user)
+
     columns = ['followers_count', 'friends_count', 'statuses_count']
     index = ['a', 'b', 'c']
     dataframe = pd.DataFrame(current_user, columns=columns, index=index)
     model = xgb.XGBClassifier(objective="binary:logistic", seed=42)
     model.load_model('trained_model.json')
     pred = model.predict(dataframe)
+
     print(twitter_user["screen_name"] + ': ' + pred[0])
+
 except Exception as err:
     print("Error:", err)
-
